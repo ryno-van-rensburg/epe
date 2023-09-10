9c95f5cf5bd7a29801836734f093717ce6b21422
GitM3 <zander@polsons.info>
2023-09-10 21:39:59 +0200

    I switched around and lost my local commits somehow

---
+++ cluedo_gui/main.cpp
@@ -0,0 +1,49 @@
+// Copyright (C) 2020 The Qt Company Ltd.
+// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR BSD-3-Clause
+
+#include <QGuiApplication>
+#include <QQmlApplicationEngine>
+#include <QQmlContext>
+#include <QTimer>
+#include <QtQuickTest/QtQuickTest>
+#include <QDebug>
+
+#include "testRunner.h"
+#include "client.h"
+
+int main(int argc, char *argv[])
+{
+    QGuiApplication app(argc, argv);
+
+
+    //qDebug() <<" NOT TESTING";
+
+    QQmlApplicationEngine engine;
+
+    Client client;
+    engine.rootContext()->setContextProperty("client",&client); // expose client to QML
+
+    if (app.arguments().contains("--runtests")) {
+        qDebug() <<"TESTING";
+        engine.addImportPath(QStringLiteral("qrc:/tests"));
+        TestRunner testObject;
+        //QTest::qExec(&testObject, app.arguments());
+        return 0;
+    }
+    client.setPlayerTurn(2);
+    engine.addImportPath(":/imports");
+    engine.load(QUrl(QLatin1String("qrc:/main.qml")));
+    if (engine.rootObjects().isEmpty())
+        return -1;
+
+    QTimer timer; // = new QTimer();
+    timer.setInterval(9000); // milliseconds
+    QObject::connect(&timer, &QTimer::timeout, [&client]() {
+        int currentTurn = client.playerTurn();
+        int nextTurn = currentTurn > 6 ? 1:(currentTurn + 1);
+        client.setPlayerTurn(nextTurn);
+    });
+    timer.start();
+
+    return app.exec();
+}
---
+++ cluedo_gui/main.qml
@@ -0,0 +1,40 @@
+// Copyright (C) 2017 The Qt Company Ltd.
+// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR BSD-3-Clause
+
+import QtQuick
+import QtQuick.Controls
+
+
+Window {
+    visible: true
+    width: 1920
+    height: 1080
+    title: qsTr("CLUEDO")
+    Loader {
+      id: screenLoader
+      anchors.fill: parent
+      sourceComponent: startScreenComponent // Load the StartScreen initially
+      }
+
+    Connections { //for switching between screens
+            target: screenLoader.item
+
+            // Connect the custom signal from StartScreen to switch to GameScreen
+            onSwitchToGameScreen: function(){
+                screenLoader.sourceComponent = gameScreenComponent;
+            }
+            onSwitchToStartScreen: function(){
+                screenLoader.sourceComponent = startScreenComponent;
+            }
+    }
+
+      // Components for StartScreen and GameScreen
+      Component {
+      id: startScreenComponent
+      StartScreen {}
+      }
+      Component {
+      id: gameScreenComponent
+      GameScreen {}
+      }
+}
