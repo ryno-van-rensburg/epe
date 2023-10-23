4fcee35232c088212ca5ba50e7a8e6deae2610ce
ryno-van-rensburg <rynovanrensburg142@gmail.com>
2023-10-23 15:22:16 +0200

    fixed turnsignal

--- cluedo_gui/client.cpp
+++ cluedo_gui/client.cpp
@@ -13,7 +13,7 @@ void Client::connectClientBroker(){
     //QObject::connect(broker, SIGNAL(cardShownToPlayer(QString,QString)));
     QObject::connect(broker, SIGNAL(playerAcceptedSignal(QString,QString,int,int)), this, SLOT(onPlayerAccepted(QString,QString,int,int)));
     //QObject::connect(broker, SIGNAL(suggestionStateUpdate(QString,QString,QString,QString)));
-    //QObject::connect(broker, SIGNAL(connectionRejectedSignal(QString)));
+    QObject::connect(broker, SIGNAL(connectionRejectedSignal(QString)), this, SLOT(emitConnectionRejected()));
     //QObject::connect(broker, SIGNAL(errorSignal(ERROR_TYPE,QString)));
     //QObject::connect(broker, SIGNAL(gameEndedSignal()));
     //QObject::connect(broker, SIGNAL(gameStartedSignal(int,QJsonArray,int,int,QList)));
